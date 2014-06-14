56898a97c5b07c3e4cb5773afe649b10a68bbc49
eller86 <skypencil@gmail.com>
2014-06-14 12:56:51 +0800

    introduced RTC.

--- stamp_checker.ino
+++ stamp_checker.ino
@@ -1,3 +1,9 @@
+#include <Wire.h>
+
+// see https://code.google.com/p/arms22/downloads/detail?name=RTC8564-001.zip
+// see http://arms22.blog91.fc2.com/blog-entry-232.html
+#include <RTC8564.h>
+
 // see http://playground.arduino.cc/Code/Time
 #include <Time.h>
 
@@ -7,7 +13,7 @@ unsigned int GREEN = 12;
 unsigned int RED = 8;
 unsigned int SERVO = 9;
 
-unsigned int LIMIT = 30; // mins
+unsigned int LIMIT = 13; // hour
 
 Servo servo;
 
@@ -19,27 +25,40 @@ void setup() {
   servo.write(180);
 
   Serial.begin(9600);
+  Rtc.begin();
 }
 
 void loop() {
-  Serial.print(minute());
+  Rtc.available();
+  Serial.print(0x2000+Rtc.years(),HEX);
+  Serial.print("/");
+  Serial.print(Rtc.months(),HEX);
+  Serial.print("/");
+  Serial.print(Rtc.days(),HEX);
   Serial.print(" ");
-  Serial.print(second());
-  Serial.println();
+  Serial.print(Rtc.hours(),HEX);
+  Serial.print(":");
+  Serial.print(Rtc.minutes(),HEX);
+  Serial.print(":");
+  Serial.println(Rtc.seconds(),HEX);
 
-  if (minute() < LIMIT) {
-    digitalWrite(GREEN, HIGH);
+  if (Rtc.hours() < LIMIT) {
+    if (Rtc.hours() == (LIMIT - 1) && Rtc.minutes() == 59) {
+      digitalWrite(GREEN, (Rtc.seconds() % 2) ? HIGH : LOW);
+    } else {
+      digitalWrite(GREEN, HIGH);
+    }
+    digitalWrite(RED, LOW);
+    servo.write(0);
+  } else if (Rtc.hours() > LIMIT) {
+    digitalWrite(GREEN, LOW);
     digitalWrite(RED, LOW);
   } else {
     digitalWrite(GREEN, LOW);
-    if (second() % 2) {
-      digitalWrite(RED, HIGH);
-    } else {
-      digitalWrite(RED, LOW);
-    }
+    digitalWrite(RED, (Rtc.seconds() % 2) ? HIGH : LOW);
 
-    if (minute() == LIMIT) {
-      servo.write(180 - second() * 2);
+    if (Rtc.minutes() == 0) {
+      servo.write(180 - Rtc.seconds() * 2);
     }
   }
   delay(200);
